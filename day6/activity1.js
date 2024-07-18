b70f59457a813f926b23ea98e120f36264359043
Krosmos <varun140704@gmail.com>
2024-07-18 14:30:11 +0530

    learning to commit through vscode

---
+++ day6/activity1.js
@@ -0,0 +1,10 @@
+
+var r = require('readline-sync');
+
+//Array creation and access
+//task 1
+let arr = [1,2,3,4,5];
+console.log(arr);
+
+//task 2
+console.log("first element:",arr[0],"\nlast element:",arr[arr.length-1]);
